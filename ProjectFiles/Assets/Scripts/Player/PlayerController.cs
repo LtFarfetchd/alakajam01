c8ded5c4ef5a45a0955631f65b600738404d18c5
Marc Wainwright <marcwainwright886@gmail.com>
2020-06-20 09:43:53 +1000

    Jumping and player movement added
    Player can move and jump now

---
+++ ProjectFiles/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Jump Force controller, adjust in editor
+    public float jumpForce;
+    //movement speed controller, adjust in editor
+    public float moveSpeed;
+    //Store the player rigidbody, takes care of itself
+    public Rigidbody2D rigBod;
+
+    private void Awake()
+    {
+        //Store the player rigidBody
+        rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Using 2D Rigidbody physics to controll jumping, adds the gravity without any extra hassle
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Debug.Log("Jump pls");
+            rigBod.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+
+        //Using transform manipulation with Time.Deltatime to smooth it over
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        }
+
+    }
+}
