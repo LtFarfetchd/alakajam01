12ada718f003529a10930d543ab869c0c196c632
Marc Wainwright <marcwainwright886@gmail.com>
2020-06-20 17:54:01 +1000

    Latching mechanic
    Whipped the latching mechanic into submission

--- ProjectFiles/Assets/Scripts/Player/PlayerController.cs
+++ ProjectFiles/Assets/Scripts/Player/PlayerController.cs
@@ -9,43 +9,39 @@ public class PlayerController : MonoBehaviour
     [HideInInspector]
     public PlayerStats play_Stats;
 
-    //Holds the player Rigidbody
     [HideInInspector]
-    public Rigidbody2D rigBod;
+    public PlayerManager play_man;
+   
     //Checks to see if the player is on a surface to jump off of
     public bool Grounded;
 
-    //Stores the player within its own script
-    public GameObject Player;
+   
 
-    //Check whether or not the player is latched to something.
-    public bool Latched;
+ 
 
     private void Awake()
     {
         play_Stats = GetComponent<PlayerStats>();
-        //Store the player rigidBody
-        rigBod = GetComponent<Rigidbody2D>();
+        play_man = GetComponent<PlayerManager>();
         Grounded = false;
-        Latched = false;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        Latched = Latched && Input.GetKey(KeyCode.LeftShift);
-        rigBod.gravityScale = Latched ? 0 : 1;
+        
 
         //Using 2D Rigidbody physics to controll jumping, adds the gravity without any extra hassle
-        if (Input.GetKeyDown(KeyCode.W) && Grounded == true) 
+        if (Input.GetKeyDown(KeyCode.W) && Grounded == true)
         {
             Debug.Log("Jump pls");
-            rigBod.AddForce(Vector2.up * play_Stats.CheckStats.player_JumpForce, ForceMode2D.Impulse);
+            play_man.rigBod.AddForce(Vector2.up * play_Stats.CheckStats.player_JumpForce, ForceMode2D.Impulse);
         }
 
 
         //Using transform manipulation with Time.Deltatime to smooth it over
-        if (Input.GetKey(KeyCode.A)) 
+        if (Input.GetKey(KeyCode.A))
         {
             transform.position = new Vector2(transform.position.x - play_Stats.CheckStats.player_MoveSpeed * Time.deltaTime, transform.position.y);
         }
@@ -54,46 +50,35 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector2(transform.position.x + play_Stats.CheckStats.player_MoveSpeed * Time.deltaTime, transform.position.y);
         }
-
-        //Should only run if latched is true
-        if (Latched == true && Input.GetKey(KeyCode.W)) 
-        {
-            Debug.Log("Rise");
-            transform.position = new Vector2(transform.position.x, transform.position.y + play_Stats.CheckStats.player_MoveSpeed * Time.deltaTime);
-        }
+   
 
     }
 
 
-  
+
 
 
     //Stops the player from spam jumping
     public void OnCollisionEnter2D(Collision2D Other)
     {
-        if (Other.gameObject.tag == "Ground"  || Other.gameObject.tag == "Platform")
+        if (Other.gameObject.tag == "Ground" || Other.gameObject.tag == "Platform")
         {
             Debug.Log("Grounded = " + Grounded);
             Grounded = true;
-
-           
         }
 
-        //Should latch the player to a wall, just testing with walls, will move on to platforms once this works
-        if (Other.gameObject.tag == "Wall" && !Latched) 
-        {
-            Latched = Input.GetKey(KeyCode.LeftShift);
-        }
+       
+
 
     }
     public void OnCollisionExit2D(Collision2D Other)
     {
-        if (Other.gameObject.tag == "Ground" ||  Other.gameObject.tag == "Platform")
+        if (Other.gameObject.tag == "Ground" || Other.gameObject.tag == "Platform")
         {
             Debug.Log("Grounded = " + Grounded);
             Grounded = false;
         }
 
-     
+
     }
 }
---
+++ ProjectFiles/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public LatchMovement LatchMove;
+    public PlayerStats PlayStat;
+    public PlayerController PlayCon;
+
+    [HideInInspector]
+    public Rigidbody2D rigBod;
+
+    //Check whether or not the player is latched to something.
+    public bool Latched;
+
+    private void Awake()
+    {
+        LatchMove = GetComponent<LatchMovement>();
+        PlayCon = GetComponent<PlayerController>();
+        PlayStat = GetComponent<PlayerStats>();
+        Latched = false;
+        rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        //LatchMove.enabled = false;
+
+        //if (PlayCon.enabled == false)
+        //{
+        //    PlayCon.enabled = true;
+        //}
+
+        if (PlayStat.enabled == false)
+        {
+            PlayStat.enabled = true;
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LatchCheck();
+        //Latched = Latched && Input.GetKey(KeyCode.LeftShift);
+        //rigBod.gravityScale = Latched ? 0 : 1;
+
+        if (Latched == true && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("trying to drop");
+            
+            
+            Debug.Log("Latched = " + Latched);
+            rigBod.constraints = RigidbodyConstraints2D.None;
+            rigBod.constraints = RigidbodyConstraints2D.FreezeRotation;
+            Latched = false;
+        
+
+        }
+    }
+
+    public void LatchCheck() 
+    {
+        switch (Latched) 
+        {
+            case false:
+                LatchMove.enabled = false;
+                rigBod.constraints = RigidbodyConstraints2D.None;
+                rigBod.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rigBod.gravityScale = 1;
+                PlayCon.enabled = true;
+                break;
+            case true:
+                LatchMove.enabled = true;
+                //rigBod.constraints = RigidbodyConstraints2D.FreezePositionX;
+                rigBod.gravityScale = 0;
+                PlayCon.enabled = false;
+                break;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D Other)
+    {
+        if (Other.gameObject.tag == "Wall" && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Latched == false)
+            {
+                Debug.Log("Latched = " + Latched);
+                rigBod.constraints = RigidbodyConstraints2D.FreezePositionX;
+                Latched = true;
+            }
+
+        }
+       
+    }
+}
