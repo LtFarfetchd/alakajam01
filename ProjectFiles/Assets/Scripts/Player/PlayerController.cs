81a16d6cb9d323f30acf33b3529d046a4ddcbd08
Marc Wainwright <marcwainwright886@gmail.com>
2020-06-20 13:25:53 +1000

    LatchTesting

--- ProjectFiles/Assets/Scripts/Player/PlayerController.cs
+++ ProjectFiles/Assets/Scripts/Player/PlayerController.cs
@@ -15,19 +15,26 @@ public class PlayerController : MonoBehaviour
     //Checks to see if the player is on a surface to jump off of
     public bool Grounded;
 
+    //Stores the player within its own script
+    public GameObject Player;
+
+    //Check whether or not the player is latched to something.
+    public bool Latched;
+
     private void Awake()
     {
         play_Stats = GetComponent<PlayerStats>();
         //Store the player rigidBody
         rigBod = GetComponent<Rigidbody2D>();
         Grounded = false;
+        Latched = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Using 2D Rigidbody physics to controll jumping, adds the gravity without any extra hassle
-        if (Input.GetKeyDown(KeyCode.Space) && Grounded == true) 
+        if (Input.GetKeyDown(KeyCode.W) && Grounded == true) 
         {
             Debug.Log("Jump pls");
             rigBod.AddForce(Vector2.up * play_Stats.CheckStats.player_JumpForce, ForceMode2D.Impulse);
@@ -45,23 +52,61 @@ public class PlayerController : MonoBehaviour
             transform.position = new Vector2(transform.position.x + play_Stats.CheckStats.player_MoveSpeed * Time.deltaTime, transform.position.y);
         }
 
+        //Should only run if latched is true
+        if (Latched == true && Input.GetKey(KeyCode.W)) 
+        {
+            Debug.Log("Rise");
+            transform.position = new Vector2(transform.position.x, transform.position.y + play_Stats.CheckStats.player_MoveSpeed * Time.deltaTime);
+        }
+
     }
+
+
+  
+
 
     //Stops the player from spam jumping
     public void OnCollisionEnter2D(Collision2D Other)
     {
-        if (Other.gameObject.tag == "Ground" || Other.gameObject.tag == "Wall" || Other.gameObject.tag == "Platform")
+        if (Other.gameObject.tag == "Ground"  || Other.gameObject.tag == "Platform")
         {
-            Debug.Log(Grounded);
+            Debug.Log("Grounded = " + Grounded);
             Grounded = true;
+
+           
         }
+
+        //Should latch the player to a wall, just testing with walls, will move on to platforms once this works
+        if (Other.gameObject.tag == "Wall" && Latched == false) 
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) 
+            {
+                Latched = true;
+                //Should for as long as you're colliding with a wall and pressing Shift down =, make the position equal the x position of that wall offset by 0.1 so you don't glitch into the wall
+                gameObject.transform.position = new Vector2(Other.gameObject.transform.position.x + 1, transform.position.y);
+                
+            }
+            
+        }
+        //Should detatch you from the wall
+        if (Other.gameObject.tag == "Wall" && Latched == true)
+        {
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                Latched = false;
+                gameObject.transform.position = new Vector2(transform.position.x, transform.position.y);
+            }
+        }
+
     }
     public void OnCollisionExit2D(Collision2D Other)
     {
-        if (Other.gameObject.tag == "Ground" || Other.gameObject.tag == "Wall" || Other.gameObject.tag == "Platform")
+        if (Other.gameObject.tag == "Ground" ||  Other.gameObject.tag == "Platform")
         {
-            Debug.Log(Grounded);
+            Debug.Log("Grounded = " + Grounded);
             Grounded = false;
         }
+
+     
     }
 }
