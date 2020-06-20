6ca17bacc6d9f222366f86d32531681aafa57dd5
Marc Wainwright <marcwainwright886@gmail.com>
2020-06-20 19:11:47 +1000

    UI Functionality

---
+++ ProjectFiles/Assets/Scripts/Utility/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    [HideInInspector]
+    public PlayerManager Play_Man;
+    [HideInInspector]
+    public LatchMovement Latch_Man;
+    [HideInInspector]
+    public PlayerController Play_Con;
+    [HideInInspector]
+    public PlayerStats Play_Stats;
+    [HideInInspector]
+    public UIManager UI_Man;
+
+
+    private void Awake()
+    {
+        Play_Man = Player.GetComponent<PlayerManager>();
+        Latch_Man = Player.GetComponent<LatchMovement>();
+        Play_Con = Player.GetComponent<PlayerController>();
+        Play_Stats = Player.GetComponent<PlayerStats>();
+        UI_Man = GetComponent<UIManager>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProjectFiles/Assets/Scripts/Utility/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameManager Game_Man;
+    public Text PlayerLives;
+    public Text PlayerHP;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Game_Man = GetComponent<GameManager>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerLives.text = "Lives: " + Game_Man.Play_Stats.CheckStats.player_Lifepoints.ToString();
+        PlayerHP.text = "Hit points: " + Game_Man.Play_Stats.CheckStats.player_HP.ToString();
+    }
+}
