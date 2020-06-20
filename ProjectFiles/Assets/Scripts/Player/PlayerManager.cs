322b451f2ec68371a9cc22af6a06e359e478cc92
Marc Wainwright <marcwainwright886@gmail.com>
2020-06-20 20:34:28 +1000

    RoofLatchInferstructure

--- ProjectFiles/Assets/Scripts/Player/PlayerManager.cs
+++ ProjectFiles/Assets/Scripts/Player/PlayerManager.cs
@@ -12,60 +12,53 @@ public class PlayerManager : MonoBehaviour
     public Rigidbody2D rigBod;
 
     //Check whether or not the player is latched to something.
-    public bool Latched;
+    public bool WallLatched;
+    public bool RoofLatched;
+    public bool CreatureLatched;
 
     private void Awake()
     {
         LatchMove = GetComponent<LatchMovement>();
         PlayCon = GetComponent<PlayerController>();
         PlayStat = GetComponent<PlayerStats>();
-        Latched = false;
+        WallLatched = false;
         rigBod = GetComponent<Rigidbody2D>();
     }
 
     private void Start()
     {
-        //LatchMove.enabled = false;
-
-        //if (PlayCon.enabled == false)
-        //{
-        //    PlayCon.enabled = true;
-        //}
-
         if (PlayStat.enabled == false)
         {
             PlayStat.enabled = true;
         }
-
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        LatchCheck();
+        //WallLatchCheck();
+        RoofLatchCheck();
         //Latched = Latched && Input.GetKey(KeyCode.LeftShift);
         //rigBod.gravityScale = Latched ? 0 : 1;
 
-        if (Latched == true && Input.GetKeyDown(KeyCode.LeftShift))
+        if (WallLatched == true && Input.GetKeyDown(KeyCode.LeftShift))
         {
-            Debug.Log("trying to drop");
-            
-            
-            Debug.Log("Latched = " + Latched);
             rigBod.constraints = RigidbodyConstraints2D.None;
             rigBod.constraints = RigidbodyConstraints2D.FreezeRotation;
-            Latched = false;
-        
+            WallLatched = false;
+        }
 
+        if (RoofLatched == true && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rigBod.constraints = RigidbodyConstraints2D.None;
+            rigBod.constraints = RigidbodyConstraints2D.FreezeRotation;
+            RoofLatched = false;
         }
     }
 
-    public void LatchCheck() 
+    public void WallLatchCheck() 
     {
-        switch (Latched) 
+        switch (WallLatched) 
         {
             case false:
                 LatchMove.enabled = false;
@@ -76,25 +69,52 @@ public class PlayerManager : MonoBehaviour
                 break;
             case true:
                 LatchMove.enabled = true;
-                //rigBod.constraints = RigidbodyConstraints2D.FreezePositionX;
+                rigBod.constraints = RigidbodyConstraints2D.FreezePositionX;
                 rigBod.gravityScale = 0;
                 PlayCon.enabled = false;
                 break;
         }
     }
 
+    public void RoofLatchCheck() 
+    {
+        switch (RoofLatched) 
+        {
+            case false:
+                LatchMove.enabled = false;
+                rigBod.constraints = RigidbodyConstraints2D.None;
+                rigBod.constraints = RigidbodyConstraints2D.FreezeRotation;
+                rigBod.gravityScale = 1;
+                PlayCon.enabled = true;
+                break;
+            case true:
+                LatchMove.enabled = true;
+                rigBod.constraints = RigidbodyConstraints2D.FreezePositionY;
+                rigBod.gravityScale = 0;
+                PlayCon.enabled = false;
+                break;
+           
+        }
+    }
+
     public void OnCollisionEnter2D(Collision2D Other)
     {
-        if (Other.gameObject.tag == "Wall" && Input.GetKey(KeyCode.LeftShift))
+        if (Other.gameObject.tag == "LatchWall" && Input.GetKey(KeyCode.LeftShift))
         {
-            if (Latched == false)
+            if (WallLatched == false)
             {
-                Debug.Log("Latched = " + Latched);
-                rigBod.constraints = RigidbodyConstraints2D.FreezePositionX;
-                Latched = true;
+                WallLatched = true;
             }
 
         }
-       
+        if (Other.gameObject.tag == "LatchRoof" && Input.GetKey(KeyCode.LeftShift))
+        {
+            if (RoofLatched == false)
+            {
+                RoofLatched = true;
+            }
+
+        }
+
     }
 }
