ccfc24d7684b295fcd9bbd252ca5fbcb5b66617b
Marc Wainwright <marcwainwright886@gmail.com>
2020-06-21 21:44:27 +1000

    Parasyte can now be a parasyte
    Added the ability to latch onto creatures and apply movement buffs, life and hitpoint buffs still await.

---
+++ ProjectFiles/Assets/Scripts/Creatures/CreatureMovement.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureMovement : MonoBehaviour
+{
+    
+    public PlayerStats Parasyte_Stats;
+   
+    public Rigidbody2D Creature_RigBod;
+    public bool Grounded;
+    public CreatureStats CStats;
+
+    public bool Flyer;
+    public bool Tank;
+    public bool Legs;
+
+    public Vector2 movement;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Parasyte_Stats = FindObjectOfType<PlayerStats>();
+    }
+    void Start()
+    {
+        //Parasyte_Stats = GetComponentInChildren<PlayerStats>();
+        
+        Creature_RigBod = GetComponent<Rigidbody2D>();
+      
+        
+        CStats = GetComponent<CreatureStats>();
+        
+        Grounded = false;
+
+        this.enabled = false;
+}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        SpeciesCheck();
+    }
+    private void FixedUpdate()
+    {
+        
+        if (Flyer == true) 
+        {
+            MovePlayer(movement);
+        }
+        if (Tank == true) 
+        {
+            TankMovement();
+        }
+    }
+    public void SpeciesCheck() 
+    {
+        switch (CStats.CreatureName)
+        {
+            case "Flyer":
+                Debug.Log("Flyer = " + Flyer);
+                Flyer = true;
+                break;
+            case "Tank":
+                Debug.Log("Flyer = " + Tank);
+                Tank = true;
+                break;
+            case "Legs":
+                Debug.Log("Flyer = " + Legs);
+                Legs = true;
+                break;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D Other)
+    {
+        if (Other.gameObject.tag == "Ground") 
+        {
+            Grounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D Other)
+    {
+        if (Other.gameObject.tag == "Ground") 
+        {
+            Grounded = false;
+        }
+    }
+
+    void MovePlayer(Vector2 direction)
+    {
+        Creature_RigBod.MovePosition((Vector2)transform.position + (direction * (CStats.MovementBuff + Parasyte_Stats.CheckStats.player_MoveSpeed) * Time.deltaTime));
+    }
+
+    public void TankMovement() 
+    {
+        if (Input.GetKeyDown(KeyCode.W) && Grounded == true)
+        {
+            //Debug.Log("Jump pls");
+            Creature_RigBod.AddForce(Vector2.up * (CStats.MovementBuff * 2), ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector2(transform.position.x - CStats.MovementBuff * Time.deltaTime, transform.position.y);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector2(transform.position.x + CStats.MovementBuff * Time.deltaTime, transform.position.y);
+
+        }
+    }
+}
--- ProjectFiles/Assets/Scripts/Player/PlayerManager.cs
+++ ProjectFiles/Assets/Scripts/Player/PlayerManager.cs
@@ -10,12 +10,21 @@ public class PlayerManager : MonoBehaviour
 
     [HideInInspector]
     public Rigidbody2D rigBod;
+    [HideInInspector]
+    public Collider2D Player_Collider;
 
     //Check whether or not the player is latched to something.
     public bool WallLatched;
     public bool RoofLatched;
     public bool CreatureLatched;
+    public bool UnlatchPls;
 
+    public CreatureStats Victim_Stats;
+    
+    [HideInInspector]
+    public CreatureMovement CreatureControl;
+
+    public GameObject Prey;
     private void Awake()
     {
         LatchMove = GetComponent<LatchMovement>();
@@ -23,6 +32,9 @@ public class PlayerManager : MonoBehaviour
         PlayStat = GetComponent<PlayerStats>();
         WallLatched = false;
         RoofLatched = false;
+        CreatureLatched = false;
+        UnlatchPls = false;
+        Player_Collider = GetComponent<Collider2D>();
         rigBod = GetComponent<Rigidbody2D>();
     }
 
@@ -32,11 +44,25 @@ public class PlayerManager : MonoBehaviour
         {
             PlayStat.enabled = true;
         }
-    }
+
+        if (Player_Collider.enabled == false)
+        {
+            Player_Collider.enabled = true;
+        }
+        }
 
     // Update is called once per frame
     void Update()
     {
+        if (CreatureLatched == true) 
+        {
+            //Victim_Stats = GetComponentInParent<CreatureStats>();
+            gameObject.transform.position = Victim_Stats.LatchPoint.transform.position;
+            rigBod.isKinematic = true;
+            Player_Collider.enabled = false;
+        }
+
+        Debug.Log("CreatureLatched = " + CreatureLatched);
 
         if (WallLatched == true && Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -65,6 +91,35 @@ public class PlayerManager : MonoBehaviour
             rigBod.gravityScale = 1;
             PlayCon.enabled = true;
             Debug.Log("Roof Latch = " + RoofLatched);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) ) 
+        {
+            if (CreatureLatched == true) 
+            {
+                Debug.Log("Unlatch pls");
+                StartCoroutine(UnlatchTimer());
+                transform.parent = null;
+                CreatureLatched = false;
+                CreatureControl.enabled = false;
+                Victim_Stats = null;
+                CreatureControl = null;
+                Player_Collider.enabled = true;
+                //Physics2D.IgnoreCollision(Player_Collider, Prey.gameObject.GetComponent<Collider2D>());
+               // Prey = null;
+
+                rigBod.isKinematic = false;
+                if (CreatureControl == true)
+                {
+                    CreatureControl.enabled = false;
+                }
+                if (PlayCon.enabled == false)
+                {
+                    PlayCon.enabled = true;
+                }
+            }
+            
 
         }
     }
@@ -99,6 +154,35 @@ public class PlayerManager : MonoBehaviour
 
         }
 
+        if (Other.gameObject.tag == "Creature" && Input.GetKey(KeyCode.LeftShift)) 
+        {
+            if (CreatureLatched == false && UnlatchPls == false) 
+            {
+                CreatureLatched = true;
+                //CreatureControl = GetComponentInParent<CreatureMovement>();
+                transform.parent = Other.transform;
+                CreatureControl = Other.gameObject.GetComponent<CreatureMovement>();
+                Victim_Stats = Other.gameObject.GetComponent<CreatureStats>();
+                gameObject.transform.position = Victim_Stats.LatchPoint.transform.position;
+                //Prey = Other.gameObject;
+                if (CreatureControl.enabled == false)
+                {
+                    CreatureControl.enabled = true;
+                }
+                if (PlayCon.enabled == true)
+                {
+                    PlayCon.enabled = false;
+                }
+
+                if (LatchMove.enabled == true)
+                {
+                    LatchMove.enabled = false;
+                }
+            }
+            
+            
+        }
+
     }
 
     private void OnCollisionExit2D(Collision2D Other)
@@ -124,4 +208,16 @@ public class PlayerManager : MonoBehaviour
             PlayCon.enabled = true;
         }
     }
+
+    public IEnumerator UnlatchTimer() 
+    {
+        if (UnlatchPls == false) 
+        {
+            UnlatchPls = true;
+        }
+        yield return new WaitForSeconds(3);
+        UnlatchPls = false;
+        StopAllCoroutines();
+    }
+
 }
